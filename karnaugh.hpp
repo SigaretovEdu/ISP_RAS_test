9b8e141463840986a260b827500b538ecd2aa077
Sigaretov <erportnyagin@edu.hse.ru>
2023-09-21 13:55:36 +0300

    review fix

--- karnaugh.hpp
+++ karnaugh.hpp
@@ -1,13 +1,9 @@
-#pragma once
-
-#include "util.hpp"
+#ifndef KARNAUGH
+#define KARNAUGH
 
 #include <algorithm>
-#include <cinttypes>
-#include <climits>
 #include <cmath>
 #include <complex>
-#include <cstddef>
 #include <iostream>
 #include <map>
 #include <math.h>
@@ -19,48 +15,48 @@
 #include <vector>
 
 struct token {
-	char tokenType;
-	char opType;
-	token *left;
-	token *right;
-	int id;
-	bool inv;
+  char tokenType;
+  char opType;
+  token *left;
+  token *right;
+  int id;
+  bool inv;
 
-	token(int a, bool f = false) {
-		tokenType = 'v';
-		id = a;
-		inv = f;
-		left = nullptr;
-		right = nullptr;
-	}
-	token(char op) {
-		tokenType = 'o';
-		opType = op;
-		left = nullptr;
-		right = nullptr;
-	}
-	token(char op, token *l, token *r) {
-		tokenType = 'o';
-		opType = op;
-		left = l;
-		right = r;
-	}
+  token(int a, bool f = false) {
+    tokenType = 'v';
+    id = a;
+    inv = f;
+    left = nullptr;
+    right = nullptr;
+  }
+  token(char op) {
+    tokenType = 'o';
+    opType = op;
+    left = nullptr;
+    right = nullptr;
+  }
+  token(char op, token *l, token *r) {
+    tokenType = 'o';
+    opType = op;
+    left = l;
+    right = r;
+  }
 
-	~token(){};
+  ~token(){};
 };
 
 struct rec {
-	size_t x1;
-	size_t y1;
-	size_t x2;
-	size_t y2;
+  size_t x1;
+  size_t y1;
+  size_t x2;
+  size_t y2;
 
-	rec(size_t y, size_t x) {
-		x1 = x;
-		y1 = y;
-		x2 = x;
-		y2 = y;
-	}
+  rec(size_t y, size_t x) {
+    x1 = x;
+    y1 = y;
+    x2 = x;
+    y2 = y;
+  }
 };
 
 inline bool operator<(const rec &l, const rec &r);
@@ -80,11 +76,15 @@ token *makeExpression(const std::vector<rec> recs, const int varNum);
 token *makeFExplression(const std::map<int, int> &ftable, const int varNum);
 
 bool calcExpression(const token *root, const int args, const int values);
-void calcExpressions(const std::vector<token *> functionsList, const int varNum);
+void compareExpressions(const std::vector<token *> functionsList, const int varNum);
 
 void printExpression(const token *root);
-void printExpressionValues(const token *root, const int &args, const int &values);
-void printEx(const std::vector<std::vector<int>> &e);
+void printExpanded(const std::vector<std::vector<int>> &e);
 void printKarnaugh(const std::vector<std::vector<int>> &karnaugh);
+std::string decToBin(int a, const size_t format = 0);
+void bprint(const std::vector<int> &a, const size_t format = 4, const char del = ' ');
+void bprint(const std::map<int, int> &m, const size_t format, const char del = ' ');
 
 void test(const int Varnum, const int seed = -1);
+
+#endif
